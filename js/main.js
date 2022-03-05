287f7f37fbfa000ab53a71e7274a9d0bb0260855
Mihailo_Grbic <mihailogrbic99@gmail.com>
2022-03-05 20:34:08 +0100

    Added dropdown menu and eraseSierpinski

--- js/main.js
+++ js/main.js
@@ -5,13 +5,17 @@ const eraseButton = document.getElementById("eraseButton");
 
 drawButton.addEventListener("click", function() {
     var depth = parseInt(document.getElementById("depth").value);
+    var constrType = document.getElementById("constrType").value
     console.log("DUBINA: " + depth);
-    if (depth == null) {
-        alert("Unesite dubinu do koje ce se crtati!");
-    } else if (depth <= 0) {
-        alert("Dubina mora biti veca od nule!");
-    } else {
-        classicSierpinski(5, 5, canvas.width - 10, canvas.height - 10, depth);
+    console.log("NACIN ISCRTAVANJA: " + constrType);
+    if (depth == null) alert("Unesite dubinu do koje ce se crtati!");
+    else if (depth <= 0) alert("Dubina mora biti veca od nule!");
+    else if (depth > 13) alert("Dubina je prevelika za iscrtavanje u realnom vremenu!");
+    else {
+        if (constrType == "classic") classicSierpinski(5, 5, canvas.width - 10, canvas.height - 10, depth);
+        else if (constrType == "erase") eraseSierpinski(5, 5, canvas.width - 10, canvas.height - 10, depth);
+        else if (constrType == "chaos") classicSierpinski(5, 5, canvas.width - 10, canvas.height - 10, depth);
+        else if (constrType == "arrowhead") classicSierpinski(5, 5, canvas.width - 10, canvas.height - 10, depth);
     }
 });
 
@@ -31,9 +35,39 @@ function classicSierpinski(X, Y, width, height, depth) {
     }
 }
 
+function eraseSierpinski(X, Y, width, height, depth) {
+    fillTriangle(X, Y, width, height, false);
+    if (depth > 1){
+        fillTriangle(X + width/4, Y + height/2, width/2, height/2, true);
+        eraseSierpinski(X, Y + height/2, width/2, height/2, depth-1)
+        eraseSierpinski(X + width/2, Y + height/2, width/2, height/2, depth-1)
+        eraseSierpinski(X + width/4, Y, width/2, height/2, depth-1)
+    }
+}
+
 function drawLine(fromX, fromY, toX, toY) {
     ctx.beginPath();
     ctx.moveTo(fromX, fromY);
     ctx.lineTo(toX, toY);
     ctx.stroke();
+}
+
+function fillTriangle(X, Y, width, height, inverse) {
+    if (inverse) {
+        ctx.fillStyle = 'white';
+        ctx.beginPath();
+        ctx.moveTo(X + width/2, Y + height);
+        ctx.lineTo(X, Y);
+        ctx.lineTo(X + width, Y);
+        ctx.fill();
+    }
+    else {
+        ctx.fillStyle = 'black';
+        ctx.beginPath();
+        ctx.moveTo(X + width/2, Y);
+        ctx.lineTo(X, Y + height);
+        ctx.lineTo(X + width, Y + height);
+        ctx.fill();
+    }
+
 }
