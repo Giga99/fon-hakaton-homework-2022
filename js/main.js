ed2ee486d7a01ea195442d9056f71eda7dccfd8e
Mihailo_Grbic <mihailogrbic99@gmail.com>
2022-03-05 19:28:34 +0100

    Added classic Sierpinski triangle drawing

--- js/main.js
+++ js/main.js
@@ -1,21 +1,39 @@
-const btn = document.getElementById("drawButton");
-btn.addEventListener("click", function() {
-    const depth = document.getElementById("depth").value;
+const canvas = document.getElementById('triangles-canvas');
+var ctx = canvas.getContext("2d");
+const drawButton = document.getElementById("drawButton");
+const eraseButton = document.getElementById("eraseButton");
+
+drawButton.addEventListener("click", function() {
+    const depth = parseInt(document.getElementById("depth").value);
     console.log("DUBINA: " + depth);
     if (depth == null) {
         alert("Unesite dubinu do koje ce se crtati!");
+    } else if (depth <= 0) {
+        alert("Dubina mora biti veca od nule!");
     } else {
-        sierpinski(depth);
+        classicSierpinski(5, 5, canvas.width - 10, canvas.height - 10, depth);
     }
 });
 
-function sierpinski(depth) {
-    drawLine(0, 0, 200, 100);
+eraseButton.addEventListener("click", function() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+function classicSierpinski(X, Y, width, height, depth) {
+    if (depth == 1){
+        drawLine(X + width/2, Y, X, Y + height);
+        drawLine(X + width/2, Y, X + width, Y + height);
+        drawLine(X, Y + height, X + width, Y + height);
+    } else {
+        classicSierpinski(X, Y + height/2, width/2, height/2, depth-1)
+        classicSierpinski(X + width/2, Y + height/2, width/2, height/2, depth-1)
+        classicSierpinski(X + width/4, Y, width/2, height/2, depth-1)
+    }
+
+    
 }
 
 function drawLine(fromX, fromY, toX, toY) {
-    var c = document.getElementById("triangles-canvas");
-    var ctx = c.getContext("2d");
     ctx.moveTo(fromX, fromY);
     ctx.lineTo(toX, toY);
     ctx.stroke();
